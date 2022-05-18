621453ae345c169a48b3ca623f28124ea2c78974
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-18 19:13:12 +0200

    created view

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,41 @@
+   <header class="header">
+      <div class="header--content">
+        <div class="header__logo-wrapper">
+          <img src="assets/dc-logo.png" alt="dc logo" />
+        </div>
+        <nav class="header__nav">
+          <ul class="header__nav-list">
+            <li class="header__nav-item">
+              <a href="#" class="header__nav-item-link">Characters</a>
+            </li>
+            <li class="header__nav-item">
+              <a href="#" class="header__nav-item-link">Comics</a>
+            </li>
+            <li class="header__nav-item">
+              <a href="#" class="header__nav-item-link">Movies</a>
+            </li>
+            <li class="header__nav-item">
+              <a href="#" class="header__nav-item-link">Tv</a>
+            </li>
+            <li class="header__nav-item">
+              <a href="#" class="header__nav-item-link">Games</a>
+            </li>
+            <li class="header__nav-item">
+              <a href="#" class="header__nav-item-link">Collectibles</a>
+            </li>
+            <li class="header__nav-item">
+              <a href="#" class="header__nav-item-link">videos</a>
+            </li>
+            <li class="header__nav-item">
+              <a href="#" class="header__nav-item-link">fans</a>
+            </li>
+            <li class="header__nav-item">
+              <a href="#" class="header__nav-item-link">news</a>
+            </li>
+            <li class="header__nav-item">
+              <a href="#" class="header__nav-item-link">shop</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
---
+++ resources/views/partials/main.blade.php
@@ -0,0 +1,15 @@
+<div class="main">
+    <div class="jumbotron"></div>
+    <div class="main-content">
+      <div class="current-series" id="currentSeries">
+        <div class="main-content-header">
+          <h2>Current Series</h2>
+        </div>
+        <div class="main__cards-list">
+        </div>
+        <div class="main-content__btn-wrapper">
+          <button>Load More</button>
+        </div>
+      </div>
+    </div>
+  </div>
