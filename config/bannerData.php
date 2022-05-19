5ca9e1394464219e8482b8e7d2fc4914863639d8
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-19 18:54:57 +0200

    Create bannerData.php

---
+++ config/bannerData.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    [
+        "id" => 1,
+        "title" => "Digital Comics",
+        "img" => "/assets/buy-comics-digital-comics.png",
+        "alt" => "Buy Digital Comics",
+    ],
+    [
+        "id" => 2,
+        "title" => "DC Merchandise",
+        "img" => "/assets/buy-comics-merchandise.png",
+        "alt" => "Buy Digital Comics Merchandise",
+    ],
+    [
+        "id" => 3,
+        "title" => "Subscribtion",
+        "img" => "/assets/buy-comics-subscriptions.png",
+        "alt" => "Buy Digital Comics Subscription",
+    ],
+    [
+        "id" => 4,
+        "title" => "Comic Shop Locator",
+        "img" => "/assets/buy-comics-shop-locator.png",
+        "alt" => "Buy Digital Comics Shop Locator",
+    ],
+    [
+        "id" => 5,
+        "title" => "DC power visa",
+        "img" => "/assets/buy-comics-shop-locator.png",
+        "alt" => "Buy DC power visa",
+    ],
+];
+
+?>
