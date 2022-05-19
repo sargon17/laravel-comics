edbf6cee0d6c848eaf77d75de8b3458d459c8635
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-19 18:10:59 +0200

    nav data loaded

---
+++ config/nav.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+    [
+        "title" => "Characters",
+        "url" => "#characters",
+        "id" => "characters",
+        "active" => false,
+    ],
+    [
+        "title" => "Comics",
+        "url" => "#currentSeries",
+        "id" => "comics",
+        "active" => true,
+    ],
+    [
+        "title" => "Movies",
+        "url" => "#movies",
+        "id" => "movies",
+        "active" => false,
+    ],
+    [
+        "title" => "Tv",
+        "url" => "#tv",
+        "id" => "tv",
+        "active" => false,
+    ],
+    [
+        "title" => "Games",
+        "url" => "#games",
+        "id" => "games",
+        "active" => false,
+    ],
+    [
+        "title" => "Collectibles",
+        "url" => "#collectibles",
+        "id" => "collectibles",
+        "active" => false,
+    ],
+    [
+        "title" => "Videos",
+        "url" => "#videos",
+        "id" => "videos",
+        "active" => false,
+    ],
+    [
+        "title" => "Fans",
+        "url" => "#fans",
+        "id" => "fans",
+        "active" => false,
+    ],
+    [
+        "title" => "News",
+        "url" => "#news",
+        "id" => "news",
+        "active" => false,
+    ],
+    [
+        "title" => "Shop",
+        "url" => "#shop",
+        "id" => "shop",
+        "active" => false,
+    ],
+];
+?>
