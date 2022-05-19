bb8270975357ee408dda0e48d48268cfe023adb5
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-19 18:16:38 +0200

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,36 +7,13 @@
         </div>
         <nav class="header__nav">
           <ul class="header__nav-list">
-            <li class="header__nav-item">
-              <a href="#" class="header__nav-item-link">Characters</a>
-            </li>
-            <li class="header__nav-item">
-              <a href="#" class="header__nav-item-link">Comics</a>
-            </li>
-            <li class="header__nav-item">
-              <a href="#" class="header__nav-item-link">Movies</a>
-            </li>
-            <li class="header__nav-item">
-              <a href="#" class="header__nav-item-link">Tv</a>
-            </li>
-            <li class="header__nav-item">
-              <a href="#" class="header__nav-item-link">Games</a>
-            </li>
-            <li class="header__nav-item">
-              <a href="#" class="header__nav-item-link">Collectibles</a>
-            </li>
-            <li class="header__nav-item">
-              <a href="#" class="header__nav-item-link">videos</a>
-            </li>
-            <li class="header__nav-item">
-              <a href="#" class="header__nav-item-link">fans</a>
-            </li>
-            <li class="header__nav-item">
-              <a href="#" class="header__nav-item-link">news</a>
-            </li>
-            <li class="header__nav-item">
-              <a href="#" class="header__nav-item-link">shop</a>
-            </li>
+              @foreach ( $nav as $item )
+                <li class="header__nav-item">
+                    <a href="{{ $item['url'] }}" class="{{ $item['active'] === true ? 'header__nav-item-link header__nav-item-link-active': 'header__nav-item-link'}}">{{ $item['title'] }}</a>
+                </li>
+
+              @endforeach
+
           </ul>
           <div class="search-input">
             <input type="text" placeholder="Search" />
