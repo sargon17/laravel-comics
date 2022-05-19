5c6d4e843b454edbba757a736cacadfb67d618a4
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-19 18:35:48 +0200

    Create social.php

---
+++ config/social.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    [
+        "name" => "facebook",
+        "url" => "#facebook",
+        "icon" => "/assets/footer-facebook.png",
+        "id" => "facebook-1",
+    ],
+    [
+        "name" => "twitter",
+        "url" => "#twitter",
+        "icon" => "/assets/footer-twitter.png",
+        "id" => "twitter-1",
+    ],
+    [
+        "name" => "youtube",
+        "url" => "#youtube",
+        "icon" => "/assets/footer-youtube.png",
+        "id" => "youtube-1",
+    ],
+    [
+        "name" => "pinterest",
+        "url" => "#pinterest",
+        "icon" => "/assets/footer-pinterest.png",
+        "id" => "pinterest-1",
+    ],
+    [
+        "name" => "periscope",
+        "url" => "#periscope",
+        "icon" => "/assets/footer-periscope.png",
+        "id" => "periscope-1",
+    ],
+];
+
+?>
