18ae35e2c79268578a28d59f629cf93950099972
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-19 18:55:03 +0200

    Update banner.blade.php

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,34 +1,11 @@
   <div class="banner">
     <div class="banner-content">
-      <div class="banner-card">
+        @foreach ( $bannerData as $item )
+        <div class="banner-card">
         <div class="banner-card__img-container">
-          <img src="assets/buy-comics-digital-comics.png" alt="alt" />
+            <img src="{{$item['img']}}" alt="{{$item['alt']}}" />
         </div>
-        <p>Digital comics</p>
+        <p>{{$item['title']}}</p>
       </div>
-      <div class="banner-card">
-        <div class="banner-card__img-container">
-          <img src="assets/buy-comics-merchandise.png" alt="alt" />
-        </div>
-        <p>Dc merchandise</p>
-      </div>
-      <div class="banner-card">
-        <div class="banner-card__img-container">
-          <img src="assets/buy-comics-subscriptions.png" alt="alt" />
-        </div>
-        <p>subscription</p>
-      </div>
-      <div class="banner-card">
-        <div class="banner-card__img-container">
-          <img src="assets/buy-comics-shop-locator.png" alt="alt" />
-        </div>
-        <p>comic shop locator</p>
-      </div>
-      <div class="banner-card">
-        <div class="banner-card__img-container">
-          <img src="assets/buy-dc-power-visa.svg" alt="alt" />
-        </div>
-        <p>dc power visa</p>
-      </div>
-    </div>
+        @endforeach
   </div>
