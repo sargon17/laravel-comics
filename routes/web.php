ac2759b23144e1aaf8448b22a0372b7cf08e9c4a
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-19 18:26:31 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
     $nav = config("nav");
+    $footerNav = config("footerNav");
     $comics = config("comics");
 
-    return view("home", compact(["comics", "nav"]));
+    return view("home", compact(["comics", "nav", "footerNav"]));
 });
 
 Route::get("/detail/{id}", function ($id) {
     $nav = config("nav");
+    $footerNav = config("footerNav");
     $comics = config("comics");
-    return view("detail", compact(["comics", "nav", "id"]));
+    return view("detail", compact(["comics", "nav", "footerNav", "id"]));
 });
